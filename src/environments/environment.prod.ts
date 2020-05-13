065cf41fe64a9ce6d608d7e49e807b355a4d4f1e
Michail Georgiopoulos <michail.georgiopoulos@gmail.com>
2020-05-13 22:07:48 +0300

    CHF: change to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  oauth_url: 'https://app-test.orderstate.com/oauth',
-  api_url: 'https://app-test.orderstate.com/api',
+  oauth_url: 'https://app.orderstate.com/oauth',
+  api_url: 'https://app.orderstate.com/api',
   client_secret: 'my-secret-token-to-change-in-production',
   client_id: 'orderstateapp',
   gmaps_key: 'AIzaSyCNaA9QlvYJadzConvAyZ1_tp0H4DkOvFk',
